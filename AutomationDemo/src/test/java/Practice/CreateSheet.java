423cf777274095105a63914899b7dcecfb8c0b45
BHAGYASHRI <BHAGYASHRI@192.168.2.8>
2018-11-30 10:54:52 +0530

    sneha

--- AutomationDemo/src/test/java/Practice/CreateSheet.java
+++ AutomationDemo/src/test/java/Practice/CreateSheet.java
@@ -13,30 +13,30 @@ public class CreateSheet {
 	
 	public static void main(String[] args) throws IOException {
 		
-		File file =  new File("D:\\WorkSpace_1\\TestFile.xlsx");
-		
-	FileInputStream input = new FileInputStream(file);
-	
-	XSSFWorkbook workbook = new XSSFWorkbook(input);
-
-	
-	//workbook.createSheet("TextSheet1");
-	XSSFSheet sheet = workbook.getSheet("TextSheet");
-	sheet.createRow(2);
-	XSSFRow row = sheet.getRow(2);
-	System.out.println("Row is created");
-	row.createCell(2);
-	XSSFCell cell = row.getCell(2);
-	cell.setCellValue("Name");
-	System.out.println("Cell value is set");
-	
-	FileOutputStream output=new FileOutputStream(file);
-	
-	workbook.write(output);
-	
-	System.out.println("Sheet is created in Workbook");
-	
-	workbook.close();
+//		File file =  new File("D:\\WorkSpace_1\\TestFile.xlsx");
+//		
+//	FileInputStream input = new FileInputStream(file);
+//	
+//	XSSFWorkbook workbook = new XSSFWorkbook(input);
+//
+//	
+//	//workbook.createSheet("TextSheet1");
+//	XSSFSheet sheet = workbook.getSheet("TextSheet");
+//	sheet.createRow(2);
+//	XSSFRow row = sheet.getRow(2);
+//	System.out.println("Row is created");
+//	row.createCell(2);
+//	XSSFCell cell = row.getCell(2);
+//	cell.setCellValue("Name");
+//	System.out.println("Cell value is set");
+//	
+//	FileOutputStream output=new FileOutputStream(file);
+//	
+//	workbook.write(output);
+//	
+//	System.out.println("Sheet is created in Workbook");
+//	
+//	workbook.close();
 	
 		
 	}
